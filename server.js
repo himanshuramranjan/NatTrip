ef8426aeef6a393d56e9dea1febda3c0343515d5
himanshuramranjan <himanshuramr@gmail.com>
2023-05-31 21:35:31 +0530

    error handling for uncaught promise and error added

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const app = require('./app');
 
 dotenv.config({ path: './config.env'});
 
+// Close Down server on any uncaught exception
+process.on('uncaughtException', (err) => {
+
+    console.log(err.message);
+    console.log('Shutting down the server due to some uncaught exception');
+
+    process.exit(1);
+});
+
 // Define DB connection URI String
 const DB = process.env.DATABASE.replace(
     '<password>', 
@@ -27,6 +36,17 @@ const PORT = process.env.PORT || 8080;
 // Start the server
 const server = app.listen(PORT, () => {
     console.log(`Server is running on the PORT: ${PORT}`);
+});
+
+// Close Down Server on Unhadled Rejections from promise
+process.on('unhandledRejection', err => {
+
+    console.log(err.message);
+    console.log('Shutting down the server due to unhandled promise rejections..');
+
+    server.close(() => {
+        process.exit(1);
+    });
 });
 
 
