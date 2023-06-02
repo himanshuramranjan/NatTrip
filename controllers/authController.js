1cacf87f899b302e3b49d5f84ab640c8581c3bb4
himanshuramranjan <himanshuramr@gmail.com>
2023-06-02 15:57:16 +0530

    role field restricted from req.body

--- controllers/authController.js
+++ controllers/authController.js
@@ -40,7 +40,13 @@ const sendJWTToken = (res, user, statusCode) => {
 // Signs up a new user
 exports.signup = catchAsyncError(async (req, res, next) => {
     
-    const user = await User.create(req.body); //[restrict role in pre save middleware]
+    const user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        photo: req.body.photo,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword
+    }); 
 
     sendJWTToken(res, user, 201);
 });
