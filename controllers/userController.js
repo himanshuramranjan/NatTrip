03cd7cc2072db439ba56d0517726ec3f8d4ff11e
himanshuramranjan <himanshuramr@gmail.com>
2023-06-03 19:49:50 +0530

    user related controllers added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,23 +1,70 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/AppError');
+const catchAsyncError = require('../utils/catchAsyncError');
+const commonController = require('./commonController');
 
-exports.getAllUsers = async(req, res, next) => {
+// filters out the field which cant be updated
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {};
 
-    const users = await User.find({});
-
-    res.status(200).json({
-        data: {
-            users
-        }
-    })
+    // loop thru each key and if its permissible add it to newObj
+    Object.keys(obj).forEach(elm => {
+        if(allowedFields.includes(elm)) newObj[elm] = obj[elm];
+    });
+    return newObj;
 }
 
-exports.getUser = async(req, res, next) => {
+// Get all the user details
+exports.getAllUsers = commonController.getAll(User);
 
-    const user = await User.findById(req.params.id);
+// Get a given user detail
+exports.getUser = commonController.getOne(User);
 
-    res.status(200).json({
-        data: {
-            user
-        }
-    })
+// Create a new user
+exports.createUser = (req, res, next) => {
+
+    res.status(500).json({
+        status: 'error',
+        message: 'Route not defined ! Please use /signup'
+    });
+}
+
+// Update a new user
+exports.updateUser = (req, res, next) => {
+
+    res.status(500).json({
+        status: 'error',
+        message: 'Route not defined ! Only User can modify its data'
+    });
+}
+
+// Delete a user
+exports.deleteUser = commonController.deleteOne(User);
+
+// Add logged in user id to the params
+exports.getMe = (req, res, next) => {
+
+    req.params.id = req.user.id;
+    next();
+}
+
+// Filter out fields and add Id to the req
+exports.updateMe = (req, res, next) => {
+    
+    if(req.body.password) {
+        return next(new AppError('This route is not for updating password', 400));
+    }
+
+    req.body = filterObj(req.body, 'name', 'photo');
+    req.params.id = req.user.id;
+
+    next();
+}
+
+exports.deleteMe = (req, res, next) => {
+
+    req.body = { active: false};
+    req.params.id = req.user.id;
+
+    next();
 }
