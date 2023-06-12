f32dde78bf99c2876f24c161bcc6f0e87dfa9437
himanshuramranjan <himanshuramr@gmail.com>
2023-06-12 11:56:11 +0530

    auth anf authorization added for tour routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,32 +1,41 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
+const authController = require('../controllers/authController');
 const reviewRouter = require('./reviewRoutes');
 const bookingRouter = require('./bookingRoutes');
 
 const router = express.Router();
 
+// nested routing for reviews and bookings
 router.use('/:tourId/reviews', reviewRouter);
 router.use('/:tourId/bookings', bookingRouter);
+
+router
+    .route('/')
+    .get(tourController.getAllTours)
+    .post(authController.restrictRoute('guide'), tourController.createTour);
+
+// Protects all the following routes from un-authenticated req
+router.use(authController.protectRoute);
 
 router.get('/top-cheap-tours',
             tourController.getTopCheapTours, 
             tourController.getAllTours
         );
 
-router.get('/tours-stats', tourController.getTourStats); 
+router 
+    .route('/:id')
+    .get(tourController.getTour);
 
-router.get('/monthly-tour-plans/:year', tourController.getMonthlyTourPlans);
-
-router
-    .route('/')
-    .get(tourController.getAllTours)
-    .post(tourController.createTour);
+// Protects all the below routes from un-authorized req
+router.use(authController.restrictRoute('guide', 'admin'));
 
 router 
     .route('/:id')
-    .get(tourController.getTour)
     .patch(tourController.updateTour)
     .delete(tourController.deleteTour);
 
+router.get('/tours-stats', tourController.getTourStats); 
+router.get('/monthly-tour-plans/:year', tourController.getMonthlyTourPlans);
 
 module.exports = router;
