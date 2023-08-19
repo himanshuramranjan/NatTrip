d5bad67d68dc7bd8465698497b0818f146839a5e
himanshuramranjan <himanshuramr@gmail.com>
2023-08-19 20:06:59 +0530

    security features added

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xssClean = require('xss-clean');
+const hpp = require('hpp');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -10,8 +15,34 @@ const AppError = require('./utils/AppError');
 // Initialize the app
 const app = express();
 
+// define max api req (to max 100 req / hour from a given IP)
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many request from this IP, please try again in 1 hour' 
+});
+
+// Middleware
+
+// set security http headers
+app.use(helmet());
+
+// limits the api request
+app.use('/api', limiter);
+
 // Parse the data from body into req.body (with 10kb data limit)
 app.use(express.json({ limit: '10kb' }));
+
+// data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// data sanitization against xss
+app.use(xssClean());
+
+// prevent http parameter pollution
+app.use(hpp({
+    whitelist: ['duration', 'ratingsQuantity', 'ratingsAverage', 'difficulty', 'maxGroupSize', 'price']
+}));
 
 // Routes
 app.use('/api/v1/nattrip/tours', tourRouter);
